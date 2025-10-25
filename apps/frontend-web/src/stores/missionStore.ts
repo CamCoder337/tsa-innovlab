36ff4740b55baa51dd4fc06fef25d53397fa3046
Navy-sama <yvan.nouafo@2026.ucac-icam.com>
2025-10-25 09:58:32 +0100

    fix: frontend

--- apps/frontend-web/src/stores/missionStore.ts
+++ apps/frontend-web/src/stores/missionStore.ts
@@ -15,6 +15,7 @@ import { missionService } from '@/services/mission.service';
 import type { PaginatedMetaResponse } from '@/types/common.types';
 import { adminService } from '@/services/admin.service';
 import { getPersistedUser } from './authStore';
+import toast from 'react-hot-toast';
 
 function getPersistedData(): Partial<MissionStoreExtended> | null {
   try {
