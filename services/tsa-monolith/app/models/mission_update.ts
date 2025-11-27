11993591f890319409ff19cce00b3aaa6c4953b7
Navy-sama <yvan.nouafo@2026.ucac-icam.com>
2025-11-28 00:38:22 +0100

    fix: deploy

--- services/tsa-monolith/app/models/mission_update.ts
+++ services/tsa-monolith/app/models/mission_update.ts
@@ -79,7 +79,7 @@ export default class MissionUpdate extends BaseModel {
   // Factory methods
   public static async createStatusUpdate(
     missionId: string,
-    transporteurId: string = '',
+    transporteurId: string | null,
     oldStatus: string | null,
     newStatus: string,
     description: string = ''
